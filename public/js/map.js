b00e7e47e61f55ec7dbd953d510fb4c4e303a8ea
Manny <equagraine@gasbuddy.com>
2017-03-18 11:14:54 -0600

    added the basics of editing (#6)

---
+++ public/js/map.js
@@ -0,0 +1,19 @@
+var  map  =  L.map('map',   {            
+    center:  [50.454722,  -104.60666700000002],
+                zoom:  13
+
+            
+});
+var points = [];
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+}).addTo(map);
+
+function onMapClick(e) {
+    $("#form").append('<input type="hidden" name="points[]" value="' + e.latlng.lat + ',' + e.latlng.lng + '" />');
+    console.log(e);
+    points.push(L.marker([e.latlng.lat, e.latlng.lng]).addTo(map))
+}
+
+map.on('click', onMapClick);
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ var router = express.Router();
 
 
 router.get('/', function(req, res) {
-  res.render('pages/index');
+    res.render('pages/index');
 });
+
+router.get('/editor', function(req, res) {
+    res.render("pages/editor");
+});
+
+router.post('/editor', function(req, res) {
+    console.log(req)
+});
+
 
 module.exports = router;
